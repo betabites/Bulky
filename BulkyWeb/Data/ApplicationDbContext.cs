eaf97b7daedd7a368ffba8b5fda0b5c1cb4ea3e4
BetaBites <jack.hawinkels@gmail.com>
2025-02-28 13:17:30 +1300

    Added database schema items

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -5,6 +5,10 @@ namespace BulkyWeb.Data;
 
 public class ApplicationDbContext: DbContext
 {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+    {
+
+    }
     public DbSet<Category> Categories { get; set; }
     
 }
