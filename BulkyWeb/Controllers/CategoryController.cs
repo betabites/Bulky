71d8db030a52e70317a56ddb0a7f3f7e3083f5b8
BetaBites <jack.hawinkels@gmail.com>
2025-02-28 14:02:59 +1300

    Fixed up some issues with the view

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,3 +1,4 @@
+using BulkyWeb.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
@@ -5,8 +6,15 @@ namespace BulkyWeb.Controllers;
 
 public class CategoryController : Controller
 {
+    private readonly ApplicationDbContext _db;
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
     public IActionResult Index()
     {
+        var objCategoryList = _db.Categories.ToList();
         return View();
     }
 }
